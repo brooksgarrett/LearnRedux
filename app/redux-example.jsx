c37576c6a51554ba35787d32daee8d7d10ca3237
Brooks Garrett <brooks+git@brooksgarrett.com>
2017-01-10 10:19:11 -0500

    Add actions for adding and removing items

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,13 +2,54 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var defaultState = {
+    name: 'Anonymous',
+    hobbies: [],
+    movies: []
+};
+
+var nextHobbyId, nextMovieId;
+nextHobbyId = nextMovieId = 1;
+var reducer = (state = defaultState, action) => {
     switch (action.type) {
         case 'CHANGE_NAME':
             return {
                 ...state,
                 name: action.name
             };
+        case 'ADD_HOBBY':
+            return {
+                ...state,
+                hobbies: [
+                    ...state.hobbies,
+                    {
+                        id: nextHobbyId++,
+                        hobby: action.hobby
+                    }
+                ]
+            }
+        case 'REMOVE_HOBBY':
+            return {
+                ...state,
+                hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+            };
+        case 'ADD_MOVIE':
+            return {
+                ...state,
+                movies: [
+                    ...state.movies,
+                    {
+                        id: nextMovieId++,
+                        title: action.title,
+                        genre: action.genre
+                    }
+                ]
+            }
+        case 'REMOVE_MOVIE':
+            return {
+                ...state,
+                movies: state.movies.filter((movie) => movie.id !== action.id )
+            }
         default:
             return state;
     }
@@ -33,4 +74,36 @@ store.dispatch({
 store.dispatch({
     type: 'CHANGE_NAME',
     name: 'Bill'
+});
+
+store.dispatch({
+    type: 'ADD_HOBBY',
+    hobby: 'Rugby'
+});
+
+store.dispatch({
+    type: 'ADD_HOBBY',
+    hobby: 'Firefighting'
+});
+
+store.dispatch({
+    type: 'ADD_MOVIE',
+    title: 'The Towering Inferno',
+    Genre: 'Action'
+});
+
+store.dispatch({
+    type: 'ADD_MOVIE',
+    title: 'Sausage Party',
+    Genre: 'Comedy'
+});
+
+store.dispatch({
+    type: 'REMOVE_HOBBY',
+    id: 2
+});
+
+store.dispatch({
+    type: 'REMOVE_MOVIE',
+    id: 1
 });
