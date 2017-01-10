453a450183e0f86ab3df951f25acf5f79675d248
Brooks Garrett <brooks+git@brooksgarrett.com>
2017-01-10 16:06:20 -0500

    Fetch a location and show loading status

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
 var redux = require('redux');
+var axios = require('axios');
 
 console.log('Starting redux example');
 
@@ -93,10 +94,51 @@ var removeMovie = (id) => {
     }
 };
 
+// Maps reducer and action generators
+// ----------------------------------
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+    switch (action.type) {
+        case 'START_LOCATION_FETCH':
+            return {
+                isFetching: true,
+                url: undefined
+            };
+        case 'COMPLETE_LOCATION_FETCH':
+            return {
+                isFetching: false,
+                url: action.url
+            };
+        default:
+            return state;
+    }
+};
+
+var startLocationFetch = () => {
+    return {
+        type: 'START_LOCATION_FETCH'
+    };
+};
+
+var completeLocationFetch = (url) => {
+    return {
+        type: 'COMPLETE_LOCATION_FETCH',
+        url
+    };
+};
+
+var fetchLocation = () => {
+    store.dispatch(startLocationFetch());
+    axios.get('http://ipinfo.io').then(function (res) {
+        var loc = res.data.loc;
+        store.dispatch(completeLocationFetch(`http://maps.google.com/?q=${loc}`));
+    });
+};
+
 var reducer = redux.combineReducers({
-    name: nameReducer,
     hobbies: hobbiesReducer,
-    movies: moviesReducer
+    map: mapReducer,
+    movies: moviesReducer,
+    name: nameReducer
 });
 
 var store = redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
@@ -105,6 +147,11 @@ var store = redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && wi
 var unsubscribe = store.subscribe(() => {
     var state = store.getState();
     console.log('State Changed', store.getState());
+    if (state.map.isFetching) {
+        document.getElementById('main').innerHTML = 'Loading...';
+    } else if (state.map.url) {
+        document.getElementById('main').innerHTML = `<a target="_blank" href="${state.map.url}">View your location</a>`;
+    }
 });
 
 // How to unsubscribe
@@ -140,3 +187,5 @@ store.dispatch({
     type: 'REMOVE_MOVIE',
     id: 1
 });
+
+fetchLocation();
